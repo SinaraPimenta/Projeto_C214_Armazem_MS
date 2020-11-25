659aede928be9a83bfb8e45907cea80b02e02bde
Mariana Helena <marianahines@hotmail.com>
2020-11-25 12:03:25 -0300

    correções

--- build.py
+++ build.py
@@ -6,15 +6,29 @@ use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
-use_plugin("python.requests")
-use_plugin("python.beautifulsoup4")
-use_plugin("python.flask")
-use_plugin("python.pymongo")
-use_plugin("python.dnspython")
+use_plugin("python.install_dependencies")
+#use_plugin("python.requests")
+#use_plugin("python.beautifulsoup4")
+#use_plugin("python.flask")
+#use_plugin("python.pymongo")
+#use_plugin("python.dnspython")
 
 name = "Armazem_MS"
-default_task = "publish"
+#default_task = "publish"
+default_task = ['install_dependencies', 'publish']
 
+
+#@init
+#def set_properties(project):
+ #  pass
+
+@init
+def initialize(project):
+    project.build_depends_on('requests')
+    project.build_depends_on('flask')
+    project.build_depends_on('pymongo')
+    project.build_depends_on('dnspython')
+    project.build_depends_on('beautifulsoup4')
 
 @init
 def set_properties(project):
--- src/unittest/model/testAdministrador.py
+++ src/unittest/model/testAdministrador.py
@@ -4,24 +4,18 @@ import administrador
 import cafeicultor 
 import unittest
 from unittest import TestCase
-#import pymongo
+import pymongo
 
 class AdministradorTest(TestCase):
     
     @classmethod
     def setUpClass(cls):
-        #cls.cliente = pymongo.MongoClient("mongodb+srv://admin:armazemMS@clusterc214.wv3t7.mongodb.net/ArmazemMS?retryWrites=true&w=majority")
-        #cls.db = cls.cliente["ArmazemMS"] 
+        cls.cliente = pymongo.MongoClient("mongodb+srv://admin:armazemMS@clusterc214.wv3t7.mongodb.net/ArmazemMS?retryWrites=true&w=majority")
+        cls.db = cls.cliente["ArmazemMS"] 
         cls.colecao = "Teste_Usuarios"
         cls.a = administrador.Administrador("Admin","admin@inatel.br","Admin#2020")
         cls.c = cafeicultor.Cafeicultor("Joao","joao123@gmail.com","teste123","3534-9965","15923678941","Itamogi","Sítio A","Banco do Brasil","8218-X","895-9")
-
-    #def setUp(self): #Antes de cada teste cadastrar um cafeicultor
-        #self.a.cadastrarCafeicultor(self.c,self.colecao)  
-
-    #def tearDown(self): 
-        #self.db.Teste_Usuarios.remove({}) #limpar a coleção após cada teste   
-        #self.a.excluirCafeicultor('joao123@gmail.com',0,self.colecao) 
+        cls.db.Teste_Usuarios.delete_many({}) #limpar a coleção antes de fazer os testes 
 
     def test_buscarCafeicultorBdVazio(self):
         resposta = self.a.buscarCafeicultores(self.colecao)
@@ -39,15 +33,33 @@ class AdministradorTest(TestCase):
     def test_cadastrarCafeicultor(self):
         self.a.cadastrarCafeicultor(self.c,self.colecao) 
         resposta = self.a.getCafeicultor(0)
-        self.assertEqual("Joao",resposta.nomeGet())  #Fazer para todos parâmetros???
+        self.assertEqual("Joao",resposta.nomeGet()) 
+        self.assertEqual("joao123@gmail.com",resposta.loginGet())
+        self.assertEqual("teste123",resposta.senhaGet()) 
+        self.assertEqual("3534-9965",resposta.telefoneGet())
+        self.assertEqual("15923678941",resposta.cpfGet())
+        self.assertEqual("Itamogi",resposta.cidadeGet())
+        self.assertEqual("Sítio A",resposta.enderecoGet())
+        self.assertEqual("Banco do Brasil",resposta.bancoGet()) 
+        self.assertEqual("8218-X",resposta.agenciaGet()) 
+        self.assertEqual("895-9",resposta.contaGet()) 
         self.a.excluirCafeicultor('joao123@gmail.com',0,self.colecao) 
 
     def test_editarCafeicultor(self):
         self.a.cadastrarCafeicultor(self.c,self.colecao) 
-        self.cafeicultorAlterado = cafeicultor.Cafeicultor("Joao Pedro","joao123@gmail.com","teste123","3534-9965","15923678941","Itamogi","Sítio A","Banco do Brasil","8218-X","895-9")
+        self.cafeicultorAlterado = cafeicultor.Cafeicultor("Joao Pedro","joao123@gmail.com","teste123","3534-9965","15923678941","Pouso Alegre","Sítio A","Bradesco","8156-0","1234-9")
         self.a.editarCafeicultor(self.cafeicultorAlterado,0,self.colecao)
         resposta = self.a.getCafeicultor(0)
-        self.assertEqual("Joao Pedro",resposta.nomeGet()) #Fazer para todos parâmetros???
+        self.assertEqual("Joao Pedro",resposta.nomeGet()) 
+        self.assertEqual("joao123@gmail.com",resposta.loginGet())
+        self.assertEqual("teste123",resposta.senhaGet()) 
+        self.assertEqual("3534-9965",resposta.telefoneGet())
+        self.assertEqual("15923678941",resposta.cpfGet())
+        self.assertEqual("Pouso Alegre",resposta.cidadeGet())
+        self.assertEqual("Sítio A",resposta.enderecoGet())
+        self.assertEqual("Bradesco",resposta.bancoGet()) 
+        self.assertEqual("8156-0",resposta.agenciaGet()) 
+        self.assertEqual("1234-9",resposta.contaGet()) 
         self.a.excluirCafeicultor('joao123@gmail.com',0,self.colecao) 
 
     def test_excluirCafeicultor(self):
